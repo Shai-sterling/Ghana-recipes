0650f48881aff55f1747fa52a42cb95f4143bcec
shaibusterling <pattiminka@gmail.com>
2021-11-04 21:39:06 +0100

    Adding resources route for recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipes/new'
-  get 'recipes/show'
-  get 'recipes/edit'
-  get 'starts/index'
-
+  
   root "starts#index"
+
+  resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

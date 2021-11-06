793ec1c3c5423b9f4e079a59a7d4eedd3276c625
shaibusterling <pattiminka@gmail.com>
2021-11-06 14:42:53 +0100

    Editing Ingredient form and new view form

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,27 +2,49 @@ class IngredientsController < ApplicationController
 
 
     def new
-
         @recipe =  Recipe.find(params[:recipe_id])
         @ingredient = Ingredient.new
+    end
+
+    def create
+        @recipe =  Recipe.find(params[:recipe_id])
+        @ingredient =  @recipe.ingredients.create(ingredient_params)
+        redirect_to @recipe
+    end
+
+
+
+    def edit
+
+        @recipe =  Recipe.find(params[:recipe_id])
+        @ingredient =  @recipe.ingredients.find(params[:id])
+
+        
 
     end
 
 
 
+    def update
+        @recipe =  Recipe.find(params[:recipe_id])
+        @ingredient =  @recipe.ingredients.find(params[:id])
+        @ingredient.update(ingredient_params)
+        redirect_to @recipe 
 
+    end
 
     private
 
     def set_recipe
-
         @recipe =  Recipe.find(params[:recipe_id])
-        
     end
 
+
+
+    
     def ingredient_params
-
         params.require(:ingredient).permit(:name, :quantity, :unit)
-
     end
+
+    
 end

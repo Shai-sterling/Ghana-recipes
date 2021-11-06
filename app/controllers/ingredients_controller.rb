b78018c603c5ae00759bafd095e80bd4b087e433
shaibusterling <pattiminka@gmail.com>
2021-11-06 15:30:12 +0100

    Adding form partial for ingredient and rendering it on the ingredients#new view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,5 @@
 class IngredientsController < ApplicationController
 
-
     def new
         @recipe =  Recipe.find(params[:recipe_id])
         @ingredient = Ingredient.new
@@ -15,57 +14,43 @@ class IngredientsController < ApplicationController
 
 
     def edit
-
         @recipe =  Recipe.find(params[:recipe_id])
         @ingredient =  @recipe.ingredients.find(params[:id])
-
-        
-
     end
 
 
 
     def update
         @recipe =  Recipe.find(params[:recipe_id])
-        @ingredient =  @recipe.ingredients.find(params[:id])
         @ingredient.update(ingredient_params)
         redirect_to @recipe 
-
     end
 
 
 
 
 
-    def destroy
-
+    def destroy  
         @recipe =  Recipe.find(params[:recipe_id])
         @ingredient =  @recipe.ingredients.find(params[:id])
-
-        
         @ingredient.destroy
         redirect_to @recipe
-
-
     end
+
+
+
+
+
 
     private
 
-    def set_recipe
-        @recipe =  Recipe.find(params[:recipe_id])
-    end
+    
 
    
-    
 
-
-    
     def ingredient_params
         params.require(:ingredient).permit(:name, :quantity, :unit)
     end
-
-
-
 
     
 end

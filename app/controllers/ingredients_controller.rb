73af9a94585497c40f8742ba91148c61082a901a
shaibusterling <pattiminka@gmail.com>
2021-11-06 03:55:53 +0100

    Adding a link to ingredients#new for creating a new ingredient on a recipe. Displaying ingredient build form in ingredients new view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,28 @@
 class IngredientsController < ApplicationController
+
+
+    def new
+
+        @recipe =  Recipe.find(params[:recipe_id])
+        @ingredient = Ingredient.new
+
+    end
+
+
+
+
+
+    private
+
+    def set_recipe
+
+        @recipe =  Recipe.find(params[:recipe_id])
+        
+    end
+
+    def ingredient_params
+
+        params.require(:ingredient).permit(:name, :quantity, :unit)
+
+    end
 end

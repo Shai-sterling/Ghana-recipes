c2419e1922299598214aa042858a0f67aaf9334b
shaibusterling <pattiminka@gmail.com>
2021-11-06 03:00:59 +0100

    Generating migration which adds recipe_id to ingredients table.. Also has_many and belongs_to to allow models to communicate with each other

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe < ApplicationRecord
 
+    has_many :ingredients, dependent: :destroy
     has_one_attached :image
 
     validates :title, presence: true
     validates :name, presence: true
     validates :description, presence: true
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_152041) do
+ActiveRecord::Schema.define(version: 2021_11_06_014805) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_11_05_152041) do
     t.string "unit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "recipe_id", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2021_11_05_152041) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "ingredients", "recipes"
 end

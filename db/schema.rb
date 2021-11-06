b8add5d818d795a2533024714fbf345c698edd59
shaibusterling <pattiminka@gmail.com>
2021-11-06 16:10:06 +0100

    Genereneating Review model with body:text and rating:integer as attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_014805) do
+ActiveRecord::Schema.define(version: 2021_11_06_150806) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2021_11_06_014805) do
     t.string "title", null: false
     t.string "name", null: false
     t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

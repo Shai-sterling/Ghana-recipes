1416be2ef8276ceda0d03eaf5edbd79bb3f8f282
shaibusterling <pattiminka@gmail.com>
2021-11-06 16:51:47 +0100

    Adding link to recipe show linking to reviews#new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,38 @@
 class ReviewsController < ApplicationController
   def new
+
+    @review = Review.new
   end
 
   def edit
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  private
+
+
+
+
+  def review_params
+
+    params.require(:review).permit(:body, :rating )
+
+
+
+  end
+
 end

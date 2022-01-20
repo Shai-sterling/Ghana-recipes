612cb0a0a59513ef830ba0e01b3b054390f1d3e3
Dennis <68560273+Shai-sterling@users.noreply.github.com>
2022-01-21 00:25:54 +0100

    Update ingredients_controller.rb

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,29 +1,26 @@
 class IngredientsController < ApplicationController
-before_acton :set_recipe
+    
+ before_action :set_recipe, only: %i[new show edit destroy update]  
+
 def new
-    @recipe =  Recipe.find(params[:recipe_id])
 end
 
 def create
-    @recipe =  Recipe.find(params[:recipe_id])
     @ingredient =  @recipe.ingredients.create(ingredient_params)
     redirect_to @recipe
 end
 
 def edit
-    @recipe =  Recipe.find(params[:recipe_id])
     @ingredient =  @recipe.ingredients.find(params[:id])
 end
 
 def update
-    @recipe =  Recipe.find(params[:recipe_id])
     @ingredient = @recipe.ingredients.find(params[:id])
     @ingredient.update(ingredient_params)
     redirect_to @recipe 
 end
 
 def destroy  
-    @ingredient =  @recipe.ingredients.find(params[:id])
     @ingredient.destroy
     redirect_to @recipe
 end

f2ab59c22bc5bcfb292d9a5e40a08a8e9be23807
Dennis <68560273+Shai-sterling@users.noreply.github.com>
2022-01-21 00:32:08 +0100

    Update recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,11 @@
 class RecipesController < ApplicationController
-  
+  before_action :set_recipe, only: %i[show edit destroy update]  
+
   def new
     @recipe = Recipe.new
   end
 
   def create
-
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
       redirect_to root_path
@@ -15,48 +15,32 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def edit    
-    @recipe = Recipe.find(params[:id])
-
   end
 
-
   def update
-    @recipe = Recipe.find(params[:id])
-
    if @recipe.update(recipe_params)
     redirect_to @recipe
    else
     render "edit"
    end
-
   end
 
-
   def destroy
-
-    @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to root_path
-
   end
 
   private
 
-  # Sets a recipe for edit, show, update and destroy
-
   def set_recipe
     @recipe = Recipe.find(params[:id])
-
   end
 
   def recipe_params
-
     params.require(:recipe).permit(:title, :name, :description, :image)
-
   end
   
 end

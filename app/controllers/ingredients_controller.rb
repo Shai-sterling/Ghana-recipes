6db247dd645231acb57d865453576e528b7b905c
Dennis <68560273+Shai-sterling@users.noreply.github.com>
2022-01-21 00:20:49 +0100

    Update ingredients_controller.rb

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,37 +1,38 @@
 class IngredientsController < ApplicationController
+before_acton :set_recipe
+def new
+    @recipe =  Recipe.find(params[:recipe_id])
+end
 
-    def new
+def create
+    @recipe =  Recipe.find(params[:recipe_id])
+    @ingredient =  @recipe.ingredients.create(ingredient_params)
+    redirect_to @recipe
+end
+
+def edit
+    @recipe =  Recipe.find(params[:recipe_id])
+    @ingredient =  @recipe.ingredients.find(params[:id])
+end
+
+def update
+    @recipe =  Recipe.find(params[:recipe_id])
+    @ingredient = @recipe.ingredients.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to @recipe 
+end
+
+def destroy  
+    @ingredient =  @recipe.ingredients.find(params[:id])
+    @ingredient.destroy
+    redirect_to @recipe
+end
+
+private
+
+    def set_recipe
         @recipe =  Recipe.find(params[:recipe_id])
     end
-
-    def create
-        @recipe =  Recipe.find(params[:recipe_id])
-        @ingredient =  @recipe.ingredients.create(ingredient_params)
-        redirect_to @recipe
-    end
-
-    def edit
-        @recipe =  Recipe.find(params[:recipe_id])
-        @ingredient =  @recipe.ingredients.find(params[:id])
-    end
-
-    def update
-        @recipe =  Recipe.find(params[:recipe_id])
-        @ingredient = @recipe.ingredients.find(params[:id])
-        @ingredient.update(ingredient_params)
-        redirect_to @recipe 
-    end
-
-
-    def destroy  
-        @recipe =  Recipe.find(params[:recipe_id])
-        @ingredient =  @recipe.ingredients.find(params[:id])
-        @ingredient.destroy
-        redirect_to @recipe
-    end
-
-
-    private
 
     def ingredient_params
         params.require(:ingredient).permit(:name, :quantity, :unit)
